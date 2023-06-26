06800a7d405751fdd550a978292b50614cc2edc1
Vyacheslav Kungin <acheros4@gmail.com>
2023-06-26 17:20:56 +0300

    initialized books app, add book model

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover_choices = [("hard", "Hard"), ("soft", "Soft")]
+    cover = models.CharField(max_length=4, choices=cover_choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.title
