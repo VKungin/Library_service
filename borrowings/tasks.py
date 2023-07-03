66a9dec67c43dc3e27e8d92624739afe79390f05
Vyacheslav Kungin <acheros4@gmail.com>
2023-07-03 18:27:50 +0300

    add message "No borrowings overdue today!"

--- borrowings/tasks.py
+++ borrowings/tasks.py
@@ -15,31 +15,13 @@ def check_overdue_borrowings():
         expected_return_date__lte=tomorrow, actual_return_date=None
     )
 
-    for borrowing in overdue_borrowings:
-        notification_message = (
-            f"The return of the book {borrowing.book.title} is overdue!"
-        )
-        send_notification(borrowing, notification_message)
-
-
-#
-# @shared_task
-# def mul(x, y):
-#     return x * y
-#
-#
-# @shared_task
-# def xsum(numbers):
-#     return sum(numbers)
-#
-#
-# @shared_task
-# def count_widgets():
-#     return Widget.objects.count()
-#
-#
-# @shared_task
-# def rename_widget(widget_id, name):
-#     w = Widget.objects.get(id=widget_id)
-#     w.name = name
-#     w.save()
+    if overdue_borrowings:
+        for borrowing in overdue_borrowings:
+            notification_message = (
+                f"The return of the book {borrowing.book.title} is overdue!"
+            )
+            send_notification(borrowing, notification_message)
+    else:
+        borrowings = Borrowing.objects.all()
+        for borrowing in borrowings:
+            send_notification(borrowing, "No borrowings overdue today!")
